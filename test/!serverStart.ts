57cd06a1dc4f8d4b0ad2cf55b9d7886e196e49c1
dangowans <d.gowans@cityssm.on.ca>
2022-02-03 15:13:17 -0500

    add puppeteer user tests

--- test/!serverStart.ts
+++ test/!serverStart.ts
@@ -1,12 +1,14 @@
-/* eslint-disable node/no-unpublished-import */
-
 import * as assert from "assert";
+
+// eslint-disable-next-line node/no-unpublished-import
+import puppeteer from "puppeteer";
 
 import * as http from "http";
 import { app } from "../app.js";
 
+import * as configFunctions from "../helpers/functions.config.js";
 
-// import { fakeViewOnlySession } from "./_globals.js";
+import { getLicences } from "../helpers/licencesDB/getLicences.js";
 
 
 describe("general-licence-manager", () => {
@@ -40,4 +42,90 @@ describe("general-licence-manager", () => {
     done();
   });
 
+  describe("databases", () => {
+    it("Ensure licences.db exists", () => {
+      assert.ok(getLicences({}, { limit: 10, offset: 0 }));
+    });
+  });
+
+  const appURL = "http://localhost:" + portNumber.toString() + configFunctions.getProperty("reverseProxy.urlPrefix");
+
+  const userName = configFunctions.getProperty("users.testing")[0];
+  const password = userName;
+
+  describe("transaction page tests", () => {
+
+    const pageTests: {
+      [pageName: string]: {
+        goto: string;
+        waitFor?: string;
+      };
+    } = {
+      reports: {
+        goto: "/reports"
+      },
+      licences: {
+        goto: "/licences",
+        waitFor: "/licences/doSearch"
+      },
+      newLicence: {
+        goto: "/licences/new"
+      }
+    };
+
+    for (const pageName of Object.keys(pageTests)) {
+
+      it("should login, navigate to " + pageName + ", and log out", (done) => {
+
+        const pageURLs = pageTests[pageName];
+
+        // eslint-disable-next-line promise/catch-or-return
+        (async() => {
+
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
+
+          // Load the login page
+
+          await page.goto(appURL);
+
+          await page.focus("#login--userName");
+          await page.type("#login--userName", userName);
+
+          await page.focus("#login--password");
+          await page.type("#login--password", password);
+
+          const loginFormElement = await page.$("#form--login");
+          await loginFormElement.evaluate((formElement: HTMLFormElement) => {
+            formElement.submit();
+          });
+
+          await page.waitForNavigation();
+
+          // Navigate to the page
+
+          const response = await page.goto(appURL + pageURLs.goto);
+
+          const responseStatus = response.status();
+
+          if (pageURLs.waitFor) {
+            await page.waitForTimeout(1000);
+          }
+
+          // Log out
+
+          await page.goto(appURL + "/logout");
+
+          await browser.close();
+
+          assert.strictEqual(responseStatus, 200);
+          done();
+
+        })()
+        .catch(() => {
+          assert.fail();
+        });
+      });
+    }
+  });
 });

5af877f3af461ac48ce26731752f31114d35401b
dangowans <d.gowans@cityssm.on.ca>
2022-03-08 15:57:09 -0500

    get outstanding batch transactions

---
+++ helpers/licencesDB/getOutstandingBatchTransactions.d.ts
@@ -0,0 +1,3 @@
+import type * as recordTypes from "../../types/recordTypes";
+export declare const getOutstandingBatchTransactions: () => recordTypes.LicenceTransaction[];
+export default getOutstandingBatchTransactions;
---
+++ helpers/licencesDB/getOutstandingBatchTransactions.ts
@@ -0,0 +1,36 @@
+import sqlite from "better-sqlite3";
+import { licencesDB as databasePath } from "../../data/databasePaths.js";
+
+import * as dateTimeFunctions from "@cityssm/expressjs-server-js/dateTimeFns.js";
+
+import type * as recordTypes from "../../types/recordTypes";
+
+
+export const getOutstandingBatchTransactions = (): recordTypes.LicenceTransaction[] => {
+
+  const database = sqlite(databasePath, {
+    readonly: true
+  });
+
+  database.function("userFn_dateIntegerToString", dateTimeFunctions.dateIntegerToString);
+  database.function("userFn_timeIntegerToString", dateTimeFunctions.timeIntegerToString);
+
+  const rows: recordTypes.LicenceTransaction[] =
+    database.prepare("select licenceId, transactionIndex," +
+      " bankTransitNumber, bankInstitutionNumber, bankAccountNumber," +
+      " batchDate, userFn_dateIntegerToString(batchDate) as batchDateString," +
+      " transactionAmount" +
+      " from LicenceTransactions" +
+      " where recordDelete_timeMillis is null" +
+      " and batchDate is not null" +
+      " and (externalReceiptNumber is null or externalReceiptNumber = '')" +
+      " order by licenceId, batchDate")
+      .all();
+
+  database.close();
+
+  return rows;
+};
+
+
+export default getOutstandingBatchTransactions;

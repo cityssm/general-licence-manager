5d34b2ae8bef5f0bf89a6d7a9c459c379fd6bd48
dangowans <d.gowans@cityssm.on.ca>
2022-05-25 14:11:04 -0400

    add test to toggle report visibility

--- cypress/integration/03-readOnly/reports.spec.js
+++ cypress/integration/03-readOnly/reports.spec.js
@@ -10,12 +10,22 @@ describe("Reports", function () {
     it("Loads page", function () {
         cy.location("pathname").should("equal", "/reports");
     });
+    it("Makes all reports visible", function () {
+        var hiddenReportsSelector = "a.is-hidden[download][href*='/reports/']";
+        cy.get(hiddenReportsSelector)
+            .should("exist");
+        cy.get("button[data-cy='panel-toggle']").each(function ($toggleButton) {
+            cy.wrap($toggleButton).click();
+        });
+        cy.get(hiddenReportsSelector)
+            .should("not.exist");
+    });
     it("Has no detectable accessibility issues", function () {
         cy.injectAxe();
         cy.checkA11y();
     });
     it("Exports all reports", function () {
-        cy.get("a[download][href*='/reports/']").each(function ($reportLink) {
+        cy.get("a:not(.is-hidden)[download][href*='/reports/']").each(function ($reportLink) {
             cy.wrap($reportLink).click();
             cy.wait(ajaxDelayMillis);
         });
--- cypress/integration/03-readOnly/reports.spec.ts
+++ cypress/integration/03-readOnly/reports.spec.ts
@@ -16,6 +16,21 @@ describe("Reports", () => {
     cy.location("pathname").should("equal", "/reports");
   });
 
+  it("Makes all reports visible", () => {
+
+    const hiddenReportsSelector = "a.is-hidden[download][href*='/reports/']";
+
+    cy.get(hiddenReportsSelector)
+      .should("exist");
+
+    cy.get("button[data-cy='panel-toggle']").each(($toggleButton) => {
+      cy.wrap($toggleButton).click();
+    });
+
+    cy.get(hiddenReportsSelector)
+      .should("not.exist");
+  });
+  
   it("Has no detectable accessibility issues", () => {
     cy.injectAxe();
     cy.checkA11y();
@@ -23,7 +38,7 @@ describe("Reports", () => {
 
   it("Exports all reports", () => {
 
-    cy.get("a[download][href*='/reports/']").each(($reportLink) => {
+    cy.get("a:not(.is-hidden)[download][href*='/reports/']").each(($reportLink) => {
       cy.wrap($reportLink).click();
       cy.wait(ajaxDelayMillis);
 

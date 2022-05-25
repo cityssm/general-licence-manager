fda458cb37a9b478ea119cd88d6e8f7f000fe809
dangowans <d.gowans@cityssm.on.ca>
2022-05-25 13:44:07 -0400

    collapse table exports
    closes #86

---
+++ public-typescript/report-search.js
@@ -0,0 +1,13 @@
+(() => {
+    const togglePanel = (clickEvent) => {
+        clickEvent.preventDefault();
+        const panelBlockElements = clickEvent.currentTarget.closest(".panel").querySelectorAll(".panel-block");
+        for (const panelBlockElement of panelBlockElements) {
+            panelBlockElement.classList.toggle("is-hidden");
+        }
+    };
+    const panelToggleButtonElements = document.querySelectorAll(".is-panel-toggle-button");
+    for (const panelToggleButtonElement of panelToggleButtonElements) {
+        panelToggleButtonElement.addEventListener("click", togglePanel);
+    }
+})();

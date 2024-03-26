2fca840e5714e14ebe864d4c8f2bb7a451592a97
Dan Gowans <dangowans@users.noreply.github.com>
2024-03-26 12:56:06 -0400

    fix pdf generation

--- handlers/licences-get/print.js
+++ handlers/licences-get/print.js
@@ -1,27 +1,23 @@
-import path from "path";
-import * as ejs from "ejs";
-import { getLicence } from "../../helpers/licencesDB/getLicence.js";
-import { getLicenceCategory } from "../../helpers/functions.cache.js";
-import * as configFunctions from "../../helpers/functions.config.js";
-import * as printFunctions from "../../helpers/functions.print.js";
-import convertHTMLToPDF from "pdf-puppeteer";
+import path from 'path';
+import * as ejs from 'ejs';
+import { getLicence } from '../../helpers/licencesDB/getLicence.js';
+import { getLicenceCategory } from '../../helpers/functions.cache.js';
+import * as configFunctions from '../../helpers/functions.config.js';
+import * as printFunctions from '../../helpers/functions.print.js';
+import convertHTMLToPDF from '@cityssm/pdf-puppeteer';
 export const handler = async (request, response, next) => {
     const licenceId = request.params.licenceId;
     const licence = getLicence(licenceId);
     if (!licence || !licence.issueDate) {
-        return next(configFunctions.getProperty("settings.licenceAlias") + " not available for printing.");
+        return next(configFunctions.getProperty('settings.licenceAlias') +
+            ' not available for printing.');
     }
     const licenceCategory = getLicenceCategory(licence.licenceCategoryKey);
-    if (!licenceCategory.printEJS || licenceCategory.printEJS === "") {
-        return next(configFunctions.getProperty("settings.licenceAlias") + " does not have a print template set.");
+    if (!licenceCategory.printEJS || licenceCategory.printEJS === '') {
+        return next(configFunctions.getProperty('settings.licenceAlias') +
+            ' does not have a print template set.');
     }
-    const reportPath = path.join(".", "print", licenceCategory.printEJS + ".ejs");
-    const pdfCallbackFunction = (pdf) => {
-        response.setHeader("Content-Disposition", "attachment;" +
-            " filename=" + configFunctions.getProperty("settings.licenceAlias").toLowerCase() + "-" + licenceId + "-" + licence.recordUpdate_timeMillis.toString() + ".pdf");
-        response.setHeader("Content-Type", "application/pdf");
-        response.send(pdf);
-    };
+    const reportPath = path.join('.', 'print', licenceCategory.printEJS + '.ejs');
     await ejs.renderFile(reportPath, {
         configFunctions,
         printFunctions,
@@ -31,12 +27,21 @@ export const handler = async (request, response, next) => {
         if (ejsError) {
             return next(ejsError);
         }
-        await convertHTMLToPDF(ejsData, pdfCallbackFunction, {
-            format: "letter",
+        const pdf = await convertHTMLToPDF(ejsData, {
+            format: 'letter',
             printBackground: true,
             preferCSSPageSize: true
         });
-        return;
+        response.setHeader('Content-Disposition', 'attachment;' +
+            ' filename=' +
+            configFunctions.getProperty('settings.licenceAlias').toLowerCase() +
+            '-' +
+            licenceId +
+            '-' +
+            licence.recordUpdate_timeMillis.toString() +
+            '.pdf');
+        response.setHeader('Content-Type', 'application/pdf');
+        response.send(pdf);
     });
 };
 export default handler;
--- handlers/licences-get/print.ts
+++ handlers/licences-get/print.ts
@@ -1,70 +1,76 @@
-import type { RequestHandler } from "express";
+import type { RequestHandler } from 'express'
 
-import path from "path";
-import * as ejs from "ejs";
+import path from 'path'
+import * as ejs from 'ejs'
 
-import { getLicence } from "../../helpers/licencesDB/getLicence.js";
-import { getLicenceCategory } from "../../helpers/functions.cache.js";
+import { getLicence } from '../../helpers/licencesDB/getLicence.js'
+import { getLicenceCategory } from '../../helpers/functions.cache.js'
 
-import * as configFunctions from "../../helpers/functions.config.js";
-import * as printFunctions from "../../helpers/functions.print.js";
+import * as configFunctions from '../../helpers/functions.config.js'
+import * as printFunctions from '../../helpers/functions.print.js'
 
-import convertHTMLToPDF from "pdf-puppeteer";
-
+import convertHTMLToPDF from '@cityssm/pdf-puppeteer'
 
 export const handler: RequestHandler = async (request, response, next) => {
+  const licenceId = request.params.licenceId
 
-  const licenceId = request.params.licenceId;
-
-  const licence = getLicence(licenceId);
+  const licence = getLicence(licenceId)
 
   if (!licence || !licence.issueDate) {
-    return next(configFunctions.getProperty("settings.licenceAlias") + " not available for printing.");
+    return next(
+      configFunctions.getProperty('settings.licenceAlias') +
+        ' not available for printing.'
+    )
   }
 
-  const licenceCategory = getLicenceCategory(licence.licenceCategoryKey);
+  const licenceCategory = getLicenceCategory(licence.licenceCategoryKey)
 
-  if (!licenceCategory.printEJS || licenceCategory.printEJS === "") {
-    return next(configFunctions.getProperty("settings.licenceAlias") + " does not have a print template set.");
+  if (!licenceCategory.printEJS || licenceCategory.printEJS === '') {
+    return next(
+      configFunctions.getProperty('settings.licenceAlias') +
+        ' does not have a print template set.'
+    )
   }
 
-  const reportPath = path.join(".", "print", licenceCategory.printEJS + ".ejs");
-
-  const pdfCallbackFunction = (pdf: Buffer) => {
-
-    response.setHeader("Content-Disposition",
-      "attachment;" +
-      " filename=" + configFunctions.getProperty("settings.licenceAlias").toLowerCase() + "-" + licenceId + "-" + licence.recordUpdate_timeMillis.toString() + ".pdf"
-    );
-
-    response.setHeader("Content-Type", "application/pdf");
-
-    response.send(pdf);
-  };
+  const reportPath = path.join('.', 'print', licenceCategory.printEJS + '.ejs')
 
   await ejs.renderFile(
-    reportPath, {
+    reportPath,
+    {
       configFunctions,
       printFunctions,
       licence,
       licenceCategory
-    }, {},
+    },
+    {},
     async (ejsError, ejsData) => {
-
       if (ejsError) {
-        return next(ejsError);
+        return next(ejsError)
       }
 
-      await convertHTMLToPDF(ejsData, pdfCallbackFunction, {
-        format: "letter",
+      const pdf = await convertHTMLToPDF(ejsData, {
+        format: 'letter',
         printBackground: true,
         preferCSSPageSize: true
-      });
+      })
 
-      return;
+      response.setHeader(
+        'Content-Disposition',
+        'attachment;' +
+          ' filename=' +
+          configFunctions.getProperty('settings.licenceAlias').toLowerCase() +
+          '-' +
+          licenceId +
+          '-' +
+          licence.recordUpdate_timeMillis.toString() +
+          '.pdf'
+      )
+  
+      response.setHeader('Content-Type', 'application/pdf')
+  
+      response.send(pdf)
     }
-  );
-};
+  )
+}
 
-
-export default handler;
+export default handler
